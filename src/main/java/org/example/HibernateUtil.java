c4953813999faadd87c0f861da768e45efdd272f
Chadith Dissanayaka <chadithdissanayaka@gmail.com>
2026-01-05 23:38:58 +0530

    initialize the project

---
+++ src/main/java/org/example/HibernateUtil.java
@@ -0,0 +1,25 @@
+package org.example;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.model.naming.ImplicitNamingStrategyJpaCompliantImpl;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+    private static SessionFactory buildSessionFactory() {
+        StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
+                //.configure("org/hibernate/example/hibernate.cfg.xml")
+                .loadProperties("hibernate.properties")
+                .build();
+
+        Metadata metadata = new MetadataSources(standardRegistry)
+                .getMetadataBuilder()
+                .applyImplicitNamingStrategy(ImplicitNamingStrategyJpaCompliantImpl.INSTANCE)
+                .build();
+
+        return metadata.getSessionFactoryBuilder()
+                .build();
+    }
+}
